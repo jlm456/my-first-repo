b24cf4b0b3c334b9962a4ff75d0f34b33f9f4072
jlm456 <jlm456@georgetown.edu>
2023-11-02 11:40:28 -0400

    Ask for user email address

--- App/email_service.py
+++ App/email_service.py
@@ -22,27 +22,27 @@ def send_email(recipient_address=SENDER_ADDRESS, subject="[Shopping Cart App] Te
     print("SUBJECT:", subject)
     print("HTML:", html_content)
 
-    #client = SendGridAPIClient(SENDGRID_API_KEY)
-    #print("CLIENT:", type(client))
+    client = SendGridAPIClient(SENDGRID_API_KEY)
+    print("CLIENT:", type(client))
 
-    #message = Mail(from_email=SENDER_ADDRESS, to_emails=recipient_address, subject=subject, html_content=html_content)
+    message = Mail(from_email=SENDER_ADDRESS, to_emails=recipient_address, subject=subject, html_content=html_content)
 
-    #try:
-    #    response = client.send(message)
+    try:
+        response = client.send(message)
 
-    #    print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
-    #    print(response.status_code) #> 202 indicates SUCCESS
-    #    print(response.body)
-    #    print(response.headers)
+        print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
+        print(response.status_code) #> 202 indicates SUCCESS
+        print(response.body)
+        print(response.headers)
 
-    #except Exception as err:
-    #    print(type(err))
-    #   print(err)
-
-
+    except Exception as err:
+        print(type(err))
+        print(err)
 
 
 
+
+user_address = input("Please enter your email address: ")
 
 
 my_content = """
@@ -63,4 +63,4 @@ my_content = """
         <li>Strawberry</li>
     </ul>
 """
-send_email(html_content=my_content)
+send_email(html_content=my_content, recipient_address=user_address)
