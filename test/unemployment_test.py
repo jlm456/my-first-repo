a57c9bf7c7d881178ede523e1c6f4f93575d0b09
jlm456 <jlm456@georgetown.edu>
2023-11-13 17:37:12 -0500

    Build fix v5

--- test/unemployment_test.py
+++ test/unemployment_test.py
@@ -2,12 +2,13 @@
 
 from app.unemployment import fetch_data
 
+
+
 def test_fetch_data():
     data = fetch_data()
-  
 
     assert isinstance(data, list)
-    #asset data[0] == {'date'}: '2023-10-01', 'value': '3.9''}
+    #assert data[0] == {'date': '2023-10-01', 'value': '3.9'}
     assert len(data) > 100
 
     assert isinstance(data[0], dict)
